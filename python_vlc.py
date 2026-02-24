5a54127622ca1f8ad7ff93a346d058a1e0f8dbdb
Bently <bentlybboy@gmail.com>
2026-02-25 12:24:46 +1300

    removed python-vlc.py as i don't even know what the point of it was anymore...

--- python_vlc.py
+++
@@ -1,8 +0,0 @@
-import vlc
-import time
-
-instance = vlc.Instance("--intf=newcurses")
-player = instance.media_player_new()
-media = instance.media_new(r"/src/videos/Test.mp4")
-player.set_media(media)
-player.play()
